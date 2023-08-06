692064a0fb8b0491b81308533a0d28bd9f934b9d
Denys Tsurkanovskyi <Denys.Tsurkanovskyi@gamanet.com>
2023-08-06 11:51:48 +0300

    changed the frameLayout height dynamically

--- app/src/main/java/com/example/weather_android/MainActivity.kt
+++ app/src/main/java/com/example/weather_android/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.weather_android
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.weather_android.databinding.GeneralPageBinding
-import com.example.weather_android.ui.theme.Weather_AndroidTheme
+
 
 class MainActivity : ComponentActivity() {
     private lateinit var baseView: GeneralPageBinding
@@ -20,5 +14,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         baseView = GeneralPageBinding.inflate(layoutInflater)
         setContentView(baseView.root)
+
+        setHeightFrame()
+    }
+
+    private fun setHeightFrame(){
+        val height = Resources.getSystem().displayMetrics.heightPixels
+        baseView.frameListDailyWeather.layoutParams.height = height - 300
     }
 }
+
